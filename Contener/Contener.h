8704c93185a03c8f81f463b06107b98ebb304a95
zimi <92szymon@gmail.com>
2018-08-30 22:53:38 +0200

    added push back prefab method

--- Contener/Contener.h
+++ Contener/Contener.h
@@ -3,9 +3,12 @@ template <typename T>
 class Contener
 {
 public:
-	Contener();
+	Contener() : m_size(0), m_element_number(0);
+	void push_back(T item);
 	~Contener();
 private:
+	int m_size;
+	int m_element_number;
 	T m_contener[];
 };
 
